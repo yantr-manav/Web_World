84558974de3d7211182adc1107088cdb34d1afbf
yantr-manav <saivamshijilla04@gmail.com>
2025-06-07 06:09:40 +0530

    date and time

---
+++ Javascript/01_bascis/08_date_and_time.js
@@ -0,0 +1,28 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date( 2025,0,23,5,3);
+// let myCreateDate = new Date("2023,0,23,5,3");
+// let myCreateDate = new Date("2025-03-12");
+// let myCreateDate = new Date("03-15-2025");
+// console.log(myCreateDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+console.log(newDate.toLocaleString())
