7a5507324f1a374c7f78daa5a170d9753a14fdd1
yantr-manav <saivamshijilla04@gmail.com>
2025-06-07 13:04:07 +0530

    Functions with objects

--- Javascript/03_basics/01_functions.js
+++ Javascript/03_basics/01_functions.js
@@ -16,7 +16,7 @@ function addTwoNumbers(num1,num2){ // parameters
 }
 
 const result = addTwoNumbers(7,22); // arguments
-console.log(result)
+// console.log(result)
 
 
 function loginUserMessage(username){
@@ -28,5 +28,38 @@ function loginUserMessage(username){
 }
 loginUserMessage("Bob"); // it won't print the output as we didn't mentioned anything to print 
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+
+
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200,300,400,2000,50000));
+
+
+const user = { 
+    username : " Adam",
+    price: " 999",
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+ handleObject({
+    username:"sam",
+    price: 399
+ })
+
+ const myNewArr = [200,300,400]
+ function returnSecondValue(getArray){
+    return getArray[1];
+ }
+ console.log(returnSecondValue(myNewArr));
+ console.log(returnSecondValue([200,300,500,1000]));
+ // both works as same ..
