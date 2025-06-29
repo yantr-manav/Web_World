0175132bc4a5e6c351566562e4f02028294e2c23
yantr-manav <saivamshijilla04@gmail.com>
2025-06-30 02:34:52 +0530

    changes in file names

--- Javascript/01_bascis/01_variables.js
+++ Javascript/01_bascis/01_variables.js
@@ -18,4 +18,35 @@ console.log(accountCity)
 
 console.log(accountId);
 
-console.table([accountEmail,accountId,accountPassword,accountCity])
+console.table([accountEmail,accountId,accountPassword,accountCity]) // to print in table format
+
+
+
+
+"use strict"; //treats all JS code as newer version 
+
+// alert(3+3) 
+//  we are using nodejs , not browser so don't expect output here
+
+// Data types
+
+// number => 2 to power 7
+// bigint
+//string => ""
+// Boolean => true/false
+// null => standalone value    Note : There's a difference between null and undefined
+// undefined =>
+// symbol =>
+
+// object
+
+let name = "Saivamshi "
+let age = 22
+let isLoggedIn = true
+
+console.log(typeof "saivamshi")
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
+
+
