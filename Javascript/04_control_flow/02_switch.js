90beb8e99adc6c455574a426bbf72804caafb0ce
yantr-manav <saivamshijilla04@gmail.com>
2025-06-30 08:00:01 +0530

    conditional statement -switch case

---
+++ Javascript/04_control_flow/02_switch.js
@@ -0,0 +1,34 @@
+// switch(key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3;
+
+switch(month){
+    case 1:
+        console.log("Jan")
+         break;
+    case 2:
+        console.log("Feb")
+         break;
+    case 3:
+        console.log("Mar")
+         break;
+    case 4:
+        console.log("April")
+         break;
+    case 5:
+        console.log("May")
+         break;
+    case 6:
+        console.log("June")
+         break;
+    default:
+        console.log("you chose a month after June")
+        break;
+}
