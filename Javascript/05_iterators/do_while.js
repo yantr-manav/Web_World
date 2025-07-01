1bc8db21ebb3bfc1681bf591c13f0c5d887b3ccc
yantr-manav <saivamshijilla04@gmail.com>
2025-07-01 10:58:57 +0530

    do-while loops

---
+++ Javascript/05_iterators/do_while.js
@@ -0,0 +1,21 @@
+// While Loop
+let index = 0 
+while(index <= 10){
+    // console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ['Flash', 'Batman', 'Superman','Ironman']
+
+let arr = 0
+while(arr< myArray.length){
+    // console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+// DO-WHILE: work done first before checking any condition
+let score = 1
+do {
+    console.log(`Value of score is : ${score}`)
+    score = score + 1;
+}while(score<=10)
