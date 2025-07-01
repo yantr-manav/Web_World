a58287107ade271766f81d545d3ec1df3269423a
yantr-manav <saivamshijilla04@gmail.com>
2025-07-01 11:26:04 +0530

    for-of loops for arrays and maps

---
+++ Javascript/05_iterators/for-of_loop.js
@@ -0,0 +1,32 @@
+// Array specific loops : for-of loop
+
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr){
+    // console.log(num)
+}
+
+
+const greetings = "Hello World!"
+for (const greet of greetings){
+    // console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States of America')
+map.set('EUR','Europe')
+map.set('JPN','Japan')
+// console.log(map)
+
+for (const [key,value] of map){
+    // console.log(key, ':-', value);
+}
+
+
+
+
+
