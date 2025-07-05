8a333609883764430d95c58128afadcdefdd5cb7
yantr-manav <saivamshijilla04@gmail.com>
2025-07-06 02:59:50 +0530

    function ( fibonacci series)

--- Learn_Rust/src/main.rs
+++ Learn_Rust/src/main.rs
@@ -1,13 +1,40 @@
 
-// Write a function to check  whether a number is even or not!
 fn main() {
     println!("{}",is_even(22)); // {} is used for dynamical variable
+    println!("{}", fib(7));
 }
 
+
+
+
+// Write a function to check  whether a number is even or not!
 fn is_even(num: i32) -> bool { //signed integer ( both positive and negative )of 32 bit number
     if num % 2 ==0 {
         return true;
         
     }
     return false;
+}
+
+
+
+
+// Function for finding fibonacci sequence
+// 0 1 1 2 3 5 8 13 ... 
+
+fn fib(num: u32)-> i32 {
+    let mut _first =0;
+    let mut _second = 1;
+    if num == 0{
+        return 0;
+    }
+    if num ==1{
+        return 1;
+    }
+    for _ in 0..(num-2){
+        let temp = _second;
+        _second = _second + _first;
+        _first = temp;
+    }
+    return _second;
 }
